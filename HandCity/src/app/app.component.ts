a74f41e4c368295e7ab5f4953f842e9386051885
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-03-31 22:15:09 -0600

    string interpolation and data binding. app class constructor

--- HandCity/src/app/app.component.ts
+++ HandCity/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HandCity';
+  // string interpolation
+  a = 3;
+  b = 5;
+  isReady = false;
+  interval;
+
+  constructor() {
+    // actualizar el valor de a cada segundo
+    setTimeout(() => {
+      this.interval = setInterval(() => { this.a++; }, 1000);
+      this.isReady = true;
+    }, 3000);
+  }
 }
